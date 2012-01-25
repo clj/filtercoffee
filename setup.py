ea8eeddecee12fd73683c8d4c3b7da60ec8206ff
Christian L. Jacobsen <christian@lyderjacobsen.org>
2012-01-25 23:35:26 +0100

    change to version 0.2 + small fixes

--- setup.py
+++ setup.py
@@ -3,18 +3,21 @@
 #from distutils.core import setup
 from setuptools import setup
 
-setup(name='FilterCoffee',
-      version='0.1',
+setup(name='filtercoffee',
+      version='0.2',
       description='A simple WSGI Middleware for compiling CoffeeScript to JavaScript on the fly',
-      #long_description='',
+      long_description=open('README.rst', 'r').read(),
       author='Christian Lyder Jacobsen',
-      author_email='christian@lyderjacobsen.org',
+      author_email='christian@lyderjacobsen.net',
       url='http://github.com/clj/filtercoffee',
+      download_url='https://github.com/downloads/clj/filtercoffee/filtercoffee-0.2.tar.gz',
       py_modules=['filtercoffee'],
       license='BSD',
       classifiers=['Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware',
                    'License :: OSI Approved :: BSD License',
                    'Intended Audience :: Developers',
+                   'Operating System :: OS Independent',
+                   'Development Status :: 3 - Alpha',
                    'Programming Language :: JavaScript',
                    'Programming Language :: Python'],
      )
